ae816dfea10c5add738fe6863f5e9e8ddb1cb36e
Paugp2000 <a24paugonpal@inspedralbes.cat>
2025-11-01 19:54:51 +0100

    CambiosPuerta

--- PuertaSFM/Assets/Scripts/PuertaController.cs
+++ PuertaSFM/Assets/Scripts/PuertaController.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.InputSystem.Processors;
 
 public class PuertaController : MonoBehaviour
 {
@@ -8,26 +10,62 @@ public class PuertaController : MonoBehaviour
     [SerializeField] GameObject puertaDer;
     public enum PuertaState { OPEN, OPENING, CLOSED, CLOSENING }
     PuertaState state = PuertaState.CLOSED;
-
+    public Vector3 rotationPuerta = new Vector3(0, 0, 1);
     private void Update()
+    {
+        UpdateState();
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("Player")== true)
+        {
+            Invoke("openingDoor", 2f);
+        }
+    }
+    private void openingDoor()
+    {
+        Debug.Log("Abriendo puerta");
+        puertaIzq.transform.rotation = Quaternion.AngleAxis(45, rotationPuerta);
+        puertaDer.transform.rotation = Quaternion.AngleAxis(-45, rotationPuerta);
+        state = PuertaState.OPEN;
+        
+    }
+    private void puertaAbierta()
+    {
+        puertaIzq.transform.rotation = Quaternion.AngleAxis(90, rotationPuerta);
+        puertaDer.transform.rotation = Quaternion.AngleAxis(-90, rotationPuerta);
+        Debug.Log("Puerta abierta");
+        state = PuertaState.CLOSENING;
+        
+    }
+    private void closeningDoor()
+    {
+        puertaIzq.transform.rotation = Quaternion.AngleAxis(45, rotationPuerta);
+        puertaDer.transform.rotation = Quaternion.AngleAxis(-45, rotationPuerta);
+        Debug.Log("Puerta cerrandose");
+        state = PuertaState.CLOSED;
+    }
+    private void puertaCerrada()
+    {
+        puertaIzq.transform.rotation = Quaternion.AngleAxis(0, rotationPuerta);
+        puertaDer.transform.rotation = Quaternion.AngleAxis(0, rotationPuerta);
+        Debug.Log("Puerta cerrada");
+        state = PuertaState.OPEN;
+    }
+    private void UpdateState()
     {
         switch (state)
         {
             case PuertaState.OPEN:
+                Invoke("puertaAbierta", 2f);
                 break;
             case PuertaState.OPENING:
                 break;
             case PuertaState.CLOSED:
                 break;
             case PuertaState.CLOSENING:
+                Invoke("closeningDoor", 4f);
                 break;
-        }
-    }
-    private void OnCollisionEnter(Collision collision)
-    {
-        if (collision.collider.CompareTag("Player"))
-        {
-            state = PuertaState.OPENING;
         }
     }
 }
